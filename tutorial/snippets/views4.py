34241f6b2e1c67c4d2d189d67b68a589b5cfe65d
Hyacinthe <hyacinthemiamo5@gmail.com>
2022-12-13 01:43:47 +0100

    Update views4.py

--- tutorial/snippets/views4.py
+++ tutorial/snippets/views4.py
@@ -1,6 +1,8 @@
+#from project
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
+#from rest_framework
 from rest_framework import mixins
 from rest_framework import generics
 
@@ -8,12 +10,19 @@ from rest_framework import generics
 class SnippetList(mixins.ListModelMixin,
                   mixins.CreateModelMixin,
                   generics.GenericAPIView):
+  
+    """
+    name        : SnippetList
+    Description : A class that can list all and create a code snippet 
+    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     
+    #function to list all the available code snippets
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
     
+    #function to create a code snippet
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
     
@@ -21,16 +30,23 @@ class SnippetDetail(mixins.RetrieveModelMixin,
                     mixins.UpdateModelMixin,
                     mixins.DestroyModelMixin,
                     generics.GenericAPIView):
+    """ 
+    name        : SnippetDetail
+    Description : This class will Retrieve, Update, and Delete a code snippet 
+    """
     
     queryset          = Snippet.objects.all()
     serializer_class  = SnippetSerializer
     
+    #method to retrieve a code snippet
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
     
+    #method to update a code snippet
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
     
+    #method to delete a code snippet
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
     

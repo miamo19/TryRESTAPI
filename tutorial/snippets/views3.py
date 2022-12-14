5081222173e016151a9e0c5f180e3dcd837712c1
Hyacinthe <hyacinthemiamo5@gmail.com>
2022-12-14 08:01:52 +0100

    Update views3.py

--- tutorial/snippets/views3.py
+++ tutorial/snippets/views3.py
@@ -13,14 +13,18 @@ from rest_framework.response import Response
 
 #       +++++classBased APIView++++++
 class SnippetList(APIView):
+    """ 
+    name: SnippetList
+    description : This class permit to list all code snippet and to create a code snippet with the different method get and post
+    """
     
-    #This function permit to list all the code snippets 
+    #This method permit to list all the code snippets 
     def get(self, request, format=None):
         snippet = Snippet.objects.all()
         serializer = SnippetSerializer(snippet, many=True)
         return Response(serializer.data)
     
-    #This function permit to create (post) a code snippet
+    #This method permit to create (post) a code snippet
     def post(self, request, format=None):
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
@@ -32,6 +36,10 @@ class SnippetList(APIView):
 snippet_view_list = SnippetList.as_view()
     
 class SnippetDetail(APIView):
+     """ 
+    name: SnippetDetail
+    description : This class permit to retrieve, update and delete a code snippet using different methods
+    """
     
     def get_object(self, pk):
         try:
@@ -39,13 +47,13 @@ class SnippetDetail(APIView):
         except Snippet.DoesNotExist:
             raise Http404
     
-    #This function permit to retrieve a code snippet
+    #This method permit to retrieve a code snippet
     def get(self, request, pk, format=None):
         snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
     
-    #This function permit to Update a code snippet
+    #This method permit to Update a code snippet
     def put(self, request, pk, format=None):
         snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
@@ -54,7 +62,7 @@ class SnippetDetail(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    #This function permit to delete (destroy) a code snippet
+    #This mehtod permit to delete (destroy) a code snippet
     def delete(self, request, pk, format=None):
         snippet = self.get_object(pk)
         snippet.delete()
